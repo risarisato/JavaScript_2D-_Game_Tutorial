0e9a0e3652542f942317b259e422b0309af6bc6a
risarisato <nqg44226@gmail.com>
2022-07-30 10:46:42 +0900

    Keyboard inputsでY軸に上下に動ける

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ window.addEventListener('load', function(){
 
     // キーボード操作入力
     class InputHandler {
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                //if(e.key === 'ArrowUp'){ ['ArrowUp', 'ArrowUp'…上矢印の長押しに対応しない
+                if((    (e.key === 'ArrowUp') ||
+                        (e.key === 'ArrowDown')
+                )&& this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                }
+                    console.log(this.game.keys);
+                });
+            //上記で「上矢印」で配列に格納されたArrowUpのみspliceで切り取る
+            // ['ArrowUp', 'ArrowUp', 'ArrowUp', 'ArrowUp', …となってしまう]
+            window.addEventListener('keyup', e =>{
+                if(this.game.keys.indexOf(e.key) > -1){
+                   this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+                console.log(this.game.keys);
+            });
+
+        }
 
     }
 
@@ -33,10 +54,15 @@ window.addEventListener('load', function(){
             this.x = 20;
             this.y = 100;
             // プレイヤー垂直速度初期値
-            this.speedY = -1;
+            this.speedY = 0;
+            //フィールドに置いた値をY速度に「this.maxSpeed」持っていくテクニック
+            this.maxSpeed = 2;
         }
         // プレイヤーの垂直方向のY速度
         update(){
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
             this.y += this.speedY;
         }
         // context引数を外からもってくるやりかた＝ctx同じ
@@ -72,6 +98,9 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.Player = new Player(this);
+            this.input = new InputHandler(this)
+            //キーボード操作を配列に格納
+            this.keys = [];
         }
         update(){
             this.Player.update();
@@ -85,6 +114,7 @@ window.addEventListener('load', function(){
 
     // アニメーションループ
     function animate() {
+        //アニメーションがクリアされる
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         game.update();
         game.draw(ctx);
